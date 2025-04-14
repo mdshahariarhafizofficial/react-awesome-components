3fcec6e60e000e426cf5b02ce2e557e06df5040c
mdshahariarhafizofficial <shahariarwebdev@gmail.com>
2025-04-14 20:42:48 +0600

    Chart added done

--- src/Components/SellingChart/SellChart.jsx
+++ src/Components/SellingChart/SellChart.jsx
@@ -6,15 +6,15 @@ const SellChart = () => {
         { month: "January", Toyota: 120, Honda: 250, Ford: 80, BMW: 400, Tesla: 600 },
         { month: "February", Toyota: 130, Honda: 110, Ford: 70, BMW: 225, Tesla: 60 },
         { month: "March", Toyota: 240, Honda: 120, Ford: 90, BMW: 450, Tesla: 180 },
-        { month: "April", Toyota: 50, Honda: 15, Ford: 85, BMW: 75, Tesla: 395 },
+        { month: "April", Toyota: 50, Honda: 15, Ford: 285, BMW: 75, Tesla: 395 },
         { month: "May", Toyota: 160, Honda: 135, Ford: 100, BMW: 80, Tesla: 110 },
         { month: "June", Toyota: 170, Honda: 200, Ford: 110, BMW: 85, Tesla: 120 },
-        { month: "July", Toyota: 280, Honda: 155, Ford: 120, BMW: 390, Tesla: 430 },
+        { month: "July", Toyota: 280, Honda: 155, Ford: 520, BMW: 390, Tesla: 430 },
         { month: "August", Toyota: 190, Honda: 360, Ford: 130, BMW: 95, Tesla: 140 },
         { month: "September", Toyota: 200, Honda: 170, Ford: 140, BMW: 100, Tesla: 150 },
-        { month: "October", Toyota: 210, Honda: 180, Ford: 150, BMW: 105, Tesla: 160 },
+        { month: "October", Toyota: 210, Honda: 180, Ford: 50, BMW: 105, Tesla: 160 },
         { month: "November", Toyota: 220, Honda: 190, Ford: 160, BMW: 110, Tesla: 570 },
-        { month: "December", Toyota: 400, Honda: 450, Ford: 170, BMW: 550, Tesla: 180 }
+        { month: "December", Toyota: 600, Honda: 450, Ford: 0, BMW: 550, Tesla: 180 }
       ];      
 
   return (
